097d0c13ea3fca55a071d5e34206192ab7db0bb5
DenaKrull <denakrull@gmail.com>
2021-12-13 22:17:54 -0500

    Homework 85

---
+++ JavaScript/85/src/index.js
@@ -0,0 +1,13 @@
+import './css/index.css';
+import $ from 'jquery';
+
+import snakehead from '../images/snakehead.png';
+
+let clickCount = 0;
+
+$('#button').click(() => {
+    $('#result').text(`I was clicked ${++clickCount} times`);
+    const snake = new Image();
+    snake.src = snakehead;
+    document.body.appendChild(snake);
+});
